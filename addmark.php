e35d3367a6e9d4ab7c92135de463907aac4186c3
Mark Gamal Hanna <122984270+Xfactor1997@users.noreply.github.com>
2023-08-05 14:12:23 +0300

    Update addmark.php

--- addmark.php
+++ addmark.php
@@ -9,7 +9,7 @@ session_start();
 				{
 					header('location: ../login.php');
 				}
-require_once('../dbcon.php');
+require_once('./dbcon.php');
 
                 
 ?>
@@ -18,7 +18,7 @@ require_once('../dbcon.php');
 
 <head>
     <title>Add Marks</title>
-    <link rel="stylesheet" href="../csss/addmark.css" type="text/css">
+    <link rel="stylesheet" href="./addmark.css" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
 
