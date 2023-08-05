179bc519226113180a55ddaf062504e147c914dc
Mark Gamal Hanna <122984270+Xfactor1997@users.noreply.github.com>
2023-08-05 14:26:59 +0300

    Update thirdstep.php

--- thirdstep.php
+++ thirdstep.php
@@ -16,7 +16,7 @@ session_start();
 
 <head>
     <title>Homepage</title>
-    <link rel="stylesheet" href="../csss/addmark.css" type="text/css">
+    <link rel="stylesheet" href="./addmark.css" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
 
@@ -28,7 +28,7 @@ session_start();
             <div class="row clearfix">
                 <img src="../image/logo_1.png" class="logo" />
                 <ul class="main-nav" animate slideInDown>
-                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="./index.php">Home</a></li>
                     <li><a href="aboutus.php">About Us</a></li>
                     <li><a href="contactus.php">Contact Us</a></li>
                 </ul>
